de41218a5b001eebd315129814e6c357869ddbec
Juliana <juliana.fulanetto@aluno.senai.br>
2025-04-15 16:12:05 -0300

    Adicionada as models da API

--- APIEscola/Data/APIEscolaContext.cs
+++ APIEscola/Data/APIEscolaContext.cs
@@ -1,5 +1,6 @@
 ﻿//Arquivo de context - responsáve  por fazer a conexão com o banco de dados
 //herdando o pacote 
+using APIEscola.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,12 +14,25 @@ namespace APIEscola.Data
         {
         }
 
+        public DbSet<Aluno> Alunos { get; set; } //Cria a tabela Alunos no banco de dados
+
+        public DbSet<Curso> Cursos { get; set; } //Cria a tabela Curso no banco de dados
+
+        public DbSet<Turma> Turmas { get; set; } //Cria a tabela Turma no banco de dados
+        public DbSet<Matricula> Matriculas { get; set; } //Cria a tabela Matricula no banco de dados
+
+
         //sobrescrever o método onmodelcreating para configurar o modelo a partir da identityDbContext
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             //chama o método OnmodelCreating da classe base para a criação das tabelas pedão
             base.OnModelCreating(modelBuilder);
             // configurar a criação de tabelas adicionais aqui 
+            modelBuilder.Entity<Aluno>().ToTable("Alunos");
+            modelBuilder.Entity<Curso>().ToTable("Cursos");
+            modelBuilder.Entity<Turma>().ToTable("Turmas");
+            modelBuilder.Entity<Matricula>().ToTable("Matriculas");
+
 
         }
     }
---
+++ APIEscola/Models/Curso.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIEscola.Models
+{
+    public class Curso
+    {
+
+        public Guid CursoId { get; set; }
+        [Required(ErrorMessage = "O campo sigla é obrigatório.")] //Campo obrigatório
+        [MaxLength(19, ErrorMessage = "A sigla deve ter no máximo 10 caracteres.")]
+        public string Sigla { get; set; }
+
+        [Required(ErrorMessage = "O campo Descrição é obrigatório.")]
+        [MaxLength(255, ErrorMessage = "A Descrição deve ter no máximo 255 caracteres.")]
+        public string Descricao { get; set; }
+
+    }
+}
